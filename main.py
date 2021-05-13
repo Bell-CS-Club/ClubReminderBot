00ec9ea8a1813ed066efafab2373e2e2a62dbb16
gen3ralpotat0 <49297102+gen3ralpotat0@users.noreply.github.com>
2021-05-12 22:16:00 -0400

    Added msg_role
    idk wrote this in like 18.1 minutes probably kinda sketch
    oh yea don't forget to add asyncio to requirements.txt

--- main.py
+++ main.py
@@ -36,6 +36,33 @@ async def shutdown(ctx):
     await ctx.send("Shutting down!")
     await client.close()
 
+@client.command()
+async def msg_role(ctx, *roles):
+    if not ctx.message.role_mentions:
+        await ctx.reply("Mention roles nerd.")
+        return
+    
+    await ctx.reply("What is your message?")
+    def get_msg(message):
+        return message.author == ctx.author and message.channel == ctx.channel
 
+    try:
+        msg = await client.wait_for('message', timeout=20.0, check=get_msg)
+    except asyncio.TimeoutError: #oh yea don't forget to add asyncio to requirements.txt
+        await ctx.reply('Enter a message or practice your typing speed.')
+        return
+    
+    members_msg = []
+    for role in ctx.message.role_mentions:
+        for member in role.members:
+            if (member not in members_msg) and (not member.bot):
+                try:
+                    print(f'Sending message from {ctx.author.mention} to {member.mention}...') #debugging ig idk how to debug
+                    await member.send(msg.content)
+                except:
+                    pass
+            members_msg.append(member)
+            print(members_msg) #debugging ig idk how to debug
+    
 if __name__ == "__main__":
     client.run(os.getenv("DISCORD_TOKEN"))
